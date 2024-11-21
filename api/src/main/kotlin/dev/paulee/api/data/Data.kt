28048f7bc12035365ffe7fff56b6e4791b2c0c68
Paulanerus <41129807+Paulanerus@users.noreply.github.com>
2024-11-21 12:01:04 +0100

    feat(data): Add new annotations for data validation and merging

--- api/src/main/kotlin/dev/paulee/api/data/Data.kt
+++ api/src/main/kotlin/dev/paulee/api/data/Data.kt
@@ -7,3 +7,15 @@ annotation class RequiresData(val name: String, val sources: Array<KClass<*>> = 
 
 @Target(AnnotationTarget.CLASS)
 annotation class DataSource(val file: String)
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class Unique
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class Index
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class NullValue(val values: Array<String>)
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class MergeWith(val field: String)
