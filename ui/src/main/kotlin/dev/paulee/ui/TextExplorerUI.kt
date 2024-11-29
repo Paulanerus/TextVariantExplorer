699c028c46ed215b2b1556c10d693b6b0793b1f5
Paul <41129807+Paulanerus@users.noreply.github.com>
2024-11-29 12:11:08 +0100

    refactor: Rename and update VariantUI to TextExplorerUI

---
+++ ui/src/main/kotlin/dev/paulee/ui/TextExplorerUI.kt
@@ -0,0 +1,68 @@
+package dev.paulee.ui
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+
+class TextExplorerUI {
+    @Composable
+    @Preview
+    private fun content() {
+        var text by remember { mutableStateOf("") }
+
+        MaterialTheme {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text("Text Explorer", fontSize = 32.sp)
+
+                TextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    placeholder = { Text("Search...") },
+                    modifier = Modifier
+                        .width(600.dp)
+                        .background(
+                            color = Color.LightGray,
+                            shape = RoundedCornerShape(24.dp),
+                        ),
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
+                )
+            }
+        }
+    }
+
+    fun start() = application {
+        val windowState =
+            rememberWindowState(position = WindowPosition.Aligned(Alignment.Center), size = DpSize(1600.dp, 900.dp))
+
+        Window(title = "TextExplorer", state = windowState, onCloseRequest = ::exitApplication) {
+            content()
+        }
+    }
+}
