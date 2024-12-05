e53ea4435efbc93f78eae2121f56d597498c1cd4
Paul <41129807+Paulanerus@users.noreply.github.com>
2024-12-05 20:04:04 +0100

    refactor: Remove ISearchable implementation from DemoPlugin

--- plugins/demo/src/main/kotlin/dev/paulee/demo/DemoPlugin.kt
+++ plugins/demo/src/main/kotlin/dev/paulee/demo/DemoPlugin.kt
@@ -2,27 +2,15 @@ package dev.paulee.demo
 
 import dev.paulee.api.data.RequiresData
 import dev.paulee.api.plugin.IPlugin
-import dev.paulee.api.plugin.ISearchable
 import dev.paulee.api.plugin.PluginMetadata
 import dev.paulee.api.plugin.PluginOrder
 
 @PluginOrder(4)
 @RequiresData(name = "demo", [Verse::class])
 @PluginMetadata(name = "Demo-Plugin", version = "1.0.0", author = "Paul")
-class DemoPlugin : IPlugin, ISearchable {
+class DemoPlugin : IPlugin {
 
     override fun init() {
         println("${greeting()} from Demo-Plugin")
-    }
-
-    override fun convertValue(fields: Array<String>): Verse? {
-        return fields.takeIf { it.size >= 2 }
-            ?.let { Verse(it[0], it[1], runCatching { it[2].toInt() }.getOrDefault(0)) }
-    }
-
-    override fun search(verse: Any?) {
-        if (verse !is Verse) return
-
-        println("Verse: ${verse.text}")
     }
 }
