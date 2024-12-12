29b06d19cd5152a820cdbff9864c369063f6dd5d
Paulanerus <41129807+Paulanerus@users.noreply.github.com>
2024-12-12 09:43:17 +0100

    refactor: Simplify DataServiceImpl logic by removing unnecessary storageProvider use block

--- core/src/main/kotlin/dev/paulee/core/data/DataServiceImpl.kt
+++ core/src/main/kotlin/dev/paulee/core/data/DataServiceImpl.kt
@@ -34,50 +34,49 @@ class DataServiceImpl(private val storageProvider: IStorageProvider) : IDataServ
 
         if (initStatus < 1) return initStatus == 0
 
-        this.storageProvider.use {
-            val indexer =
-                runCatching {
-                    Indexer(
-                        path.resolve("index/${dataInfo.name}"),
-                        dataInfo.sources
-                    )
-                }.getOrElse { exception ->
-                    println("Failed to create index for ${dataInfo.name}")
-                    return false
-                }
-
-            dataInfo.sources.forEach { clazz ->
-                val file = clazz.findAnnotation<DataSource>()?.file
-
-                if (file.isNullOrEmpty()) {
-                    println("No data source provided for ${clazz.simpleName}")
-                    return@forEach
-                }
-
-                val sourcePath = path.resolve(file.let { if (it.endsWith(".csv")) it else "$it.csv" })
-
-                if (!sourcePath.exists()) {
-                    println("Source file '$sourcePath' not found")
-                    return@forEach
-                }
-
-                val hasIdentifier =
-                    clazz.primaryConstructor?.parameters.orEmpty().any { it.findAnnotation<Unique>()?.identify == true }
-
-                val idGenerator = generateSequence(1L) { it + 1 }.iterator()
-
-                BufferedCSVReader(sourcePath).readLines { lines ->
-                    val entries = lines.map { line ->
-                        if (hasIdentifier) line
-                        else line + ("${file}_ag_id" to idGenerator.next().toString())
-                    }
-
-                    indexer.indexEntries(file, entries)
-                    this.storageProvider.insert("${dataInfo.name}.$file", entries)
-                }
+        val indexer =
+            runCatching {
+                Indexer(
+                    path.resolve("index/${dataInfo.name}"),
+                    dataInfo.sources
+                )
+            }.getOrElse { exception ->
+                println("Failed to create index for ${dataInfo.name}")
+                return false
             }
-            indexer.close()
+
+        dataInfo.sources.forEach { clazz ->
+            val file = clazz.findAnnotation<DataSource>()?.file
+
+            if (file.isNullOrEmpty()) {
+                println("No data source provided for ${clazz.simpleName}")
+                return@forEach
+            }
+
+            val sourcePath = path.resolve(file.let { if (it.endsWith(".csv")) it else "$it.csv" })
+
+            if (!sourcePath.exists()) {
+                println("Source file '$sourcePath' not found")
+                return@forEach
+            }
+
+            val hasIdentifier =
+                clazz.primaryConstructor?.parameters.orEmpty().any { it.findAnnotation<Unique>()?.identify == true }
+
+            val idGenerator = generateSequence(1L) { it + 1 }.iterator()
+
+            BufferedCSVReader(sourcePath).readLines { lines ->
+                val entries = lines.map { line ->
+                    if (hasIdentifier) line
+                    else line + ("${file}_ag_id" to idGenerator.next().toString())
+                }
+
+                indexer.indexEntries(file, entries)
+                this.storageProvider.insert("${dataInfo.name}.$file", entries)
+            }
         }
+        indexer.close()
+
         return true
     }
 
