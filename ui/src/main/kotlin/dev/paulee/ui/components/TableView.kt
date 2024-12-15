da07f6aea1eedb4c2e4ebf917b4c65829ef501bf
Paul <41129807+Paulanerus@users.noreply.github.com>
2024-12-15 19:37:37 +0100

    refactor: Updated TableView to enhance modularity

--- ui/src/main/kotlin/dev/paulee/ui/components/TableView.kt
+++ ui/src/main/kotlin/dev/paulee/ui/components/TableView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -25,6 +26,7 @@ import java.awt.Cursor
 
 @Composable
 fun TableView(
+    modifier: Modifier = Modifier,
     columns: List<String>,
     data: List<List<String>>,
     onRowSelect: (Set<List<String>>) -> Unit,
@@ -39,126 +41,129 @@ fun TableView(
         columns.map { mutableStateOf(175.dp) }
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
-            IconButton(
-                onClick = {
-                    selectedRows.value = emptySet()
-                    onRowSelect(emptySet())
-                }, modifier = Modifier.align(Alignment.CenterStart), enabled = selectedRows.value.isNotEmpty()
-            ) {
-                Icon(Icons.Default.Delete, contentDescription = "Delete")
-            }
+    Box(modifier = modifier) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
+                IconButton(
+                    onClick = {
+                        selectedRows.value = emptySet()
+                        onRowSelect(emptySet())
+                    }, modifier = Modifier.align(Alignment.CenterStart), enabled = selectedRows.value.isNotEmpty()
+                ) {
+                    Icon(Icons.Default.Delete, contentDescription = "Delete")
+                }
 
-            Row(horizontalArrangement = Arrangement.spacedBy(16.dp), modifier = Modifier.align(Alignment.Center)) {
-                columns.forEachIndexed { index, column ->
-                    Button(onClick = {
-                        if (hiddenColumns.value.contains(index))
-                            hiddenColumns.value = hiddenColumns.value - index
-                        else
-                            hiddenColumns.value = hiddenColumns.value + index
+                Row(horizontalArrangement = Arrangement.spacedBy(16.dp), modifier = Modifier.align(Alignment.Center)) {
+                    columns.forEachIndexed { index, column ->
+                        Button(onClick = {
+                            if (hiddenColumns.value.contains(index))
+                                hiddenColumns.value = hiddenColumns.value - index
+                            else
+                                hiddenColumns.value = hiddenColumns.value + index
 
-                    }, colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)) {
-                        Text(column)
+                        }, colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)) {
+                            Text(column)
+                        }
                     }
+                }
+
+                Button(
+                    onClick = clicked,
+                    enabled = selectedRows.value.isNotEmpty(),
+                    modifier = Modifier.width(120.dp).align(Alignment.CenterEnd)
+                ) {
+                    if (selectedRows.value.size <= 1) Text("View")
+                    else Text("View Diff")
                 }
             }
 
-            Button(
-                onClick = clicked,
-                enabled = selectedRows.value.isNotEmpty(),
-                modifier = Modifier.width(120.dp).align(Alignment.CenterEnd)
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth().horizontalScroll(scrollState).background(Color.Gray)
             ) {
-                if (selectedRows.value.size <= 1) Text("View")
-                else Text("View Diff")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth().horizontalScroll(scrollState).background(Color.Gray)
-        ) {
-            columns.forEachIndexed { index, columnName ->
-                if (hiddenColumns.value.contains(index)) return@forEachIndexed
-
-                Box(
-                    modifier = Modifier.height(IntrinsicSize.Min).width(columnWidths[index].value).drawBehind {
-                        val strokeWidth = 2.dp.toPx()
-                        val halfStrokeWidth = strokeWidth / 2
-                        drawLine(
-                            color = Color.DarkGray,
-                            start = Offset(size.width - halfStrokeWidth, 0f),
-                            end = Offset(size.width - halfStrokeWidth, size.height),
-                            strokeWidth = strokeWidth
-                        )
-                    }
-                ) {
-                    Text(
-                        text = columnName,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(horizontal = 4.dp, vertical = 8.dp)
-                    )
+                columns.forEachIndexed { index, columnName ->
+                    if (hiddenColumns.value.contains(index)) return@forEachIndexed
 
                     Box(
-                        modifier = Modifier.align(Alignment.CenterEnd).width(4.dp)
-                            .fillMaxHeight()
-                            .pointerHoverIcon(PointerIcon(Cursor(Cursor.E_RESIZE_CURSOR)))
-                            .pointerInput(Unit) {
-                                detectDragGestures { change, dragAmount ->
-                                    change.consume()
-                                    val newWidth =
-                                        (columnWidths[index].value + dragAmount.x.dp).coerceAtLeast(50.dp)
-                                    columnWidths[index].value = newWidth
-                                }
-                            })
+                        modifier = Modifier.height(IntrinsicSize.Min).width(columnWidths[index].value).drawBehind {
+                            val strokeWidth = 2.dp.toPx()
+                            val halfStrokeWidth = strokeWidth / 2
+                            drawLine(
+                                color = Color.DarkGray,
+                                start = Offset(size.width - halfStrokeWidth, 0f),
+                                end = Offset(size.width - halfStrokeWidth, size.height),
+                                strokeWidth = strokeWidth
+                            )
+                        }
+                    ) {
+                        Text(
+                            text = columnName,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(horizontal = 4.dp, vertical = 8.dp)
+                        )
+
+                        Box(
+                            modifier = Modifier.align(Alignment.CenterEnd).width(4.dp)
+                                .fillMaxHeight()
+                                .pointerHoverIcon(PointerIcon(Cursor(Cursor.E_RESIZE_CURSOR)))
+                                .pointerInput(Unit) {
+                                    detectDragGestures { change, dragAmount ->
+                                        change.consume()
+                                        val newWidth =
+                                            (columnWidths[index].value + dragAmount.x.dp).coerceAtLeast(50.dp)
+                                        columnWidths[index].value = newWidth
+                                    }
+                                })
+                    }
                 }
             }
-        }
 
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
             Box(
-                modifier = Modifier.horizontalScroll(scrollState)
+                modifier = Modifier.fillMaxSize()
             ) {
-                LazyColumn(state = verticalScrollState) {
-                    items(data.size) { rowIndex ->
-                        val row = data[rowIndex]
-                        Row(
-                            modifier = Modifier.fillMaxWidth().clickable {
-                                val selected = if (selectedRows.value.contains(rowIndex)) {
-                                    selectedRows.value - rowIndex
-                                } else {
-                                    selectedRows.value + rowIndex
+                Box(
+                    modifier = Modifier.horizontalScroll(scrollState)
+                ) {
+                    LazyColumn(state = verticalScrollState) {
+                        items(data.size) { rowIndex ->
+                            val row = data[rowIndex]
+                            Row(
+                                modifier = Modifier.fillMaxWidth().clickable {
+                                    val selected = if (selectedRows.value.contains(rowIndex)) {
+                                        selectedRows.value - rowIndex
+                                    } else {
+                                        selectedRows.value + rowIndex
+                                    }
+                                    selectedRows.value = selected
+                                    onRowSelect(selected.map { data[it] }.toSet())
                                 }
-                                selectedRows.value = selected
-                                onRowSelect(selected.map { data[it] }.toSet())
-                            }
-                                .background(if (selectedRows.value.contains(rowIndex)) Color.LightGray else Color.Transparent)
-                                .padding(vertical = 8.dp),
-                        ) {
-                            row.forEachIndexed { colIndex, cell ->
-                                if (hiddenColumns.value.contains(colIndex)) return@forEachIndexed
+                                    .background(if (selectedRows.value.contains(rowIndex)) Color.LightGray else Color.Transparent)
+                                    .padding(vertical = 8.dp),
+                            ) {
+                                row.forEachIndexed { colIndex, cell ->
+                                    if (hiddenColumns.value.contains(colIndex)) return@forEachIndexed
 
-                                Text(
-                                    text = cell,
-                                    modifier = Modifier.width(columnWidths[colIndex].value).padding(horizontal = 4.dp)
-                                )
+                                    Text(
+                                        text = cell,
+                                        modifier = Modifier.width(columnWidths[colIndex].value)
+                                            .padding(horizontal = 4.dp)
+                                    )
+                                }
                             }
                         }
                     }
                 }
+
+                VerticalScrollbar(
+                    adapter = rememberScrollbarAdapter(verticalScrollState),
+                    modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight().padding(end = 4.dp)
+                )
+
+                HorizontalScrollbar(
+                    adapter = rememberScrollbarAdapter(scrollState), modifier = Modifier.align(Alignment.BottomCenter)
+                )
             }
-
-            VerticalScrollbar(
-                adapter = rememberScrollbarAdapter(verticalScrollState),
-                modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight().padding(end = 4.dp)
-            )
-
-            HorizontalScrollbar(
-                adapter = rememberScrollbarAdapter(scrollState), modifier = Modifier.align(Alignment.BottomCenter)
-            )
         }
     }
 }
