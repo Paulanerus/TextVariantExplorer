54c7f88346f7ddc43ba0e6b97ecdbd755060d77a
Paul <41129807+Paulanerus@users.noreply.github.com>
2025-01-02 21:17:15 +0100

    feat: Added new Taggable api interface and ViewFilter annotation

--- api/src/main/kotlin/dev/paulee/api/plugin/Plugin.kt
+++ api/src/main/kotlin/dev/paulee/api/plugin/Plugin.kt
@@ -1,5 +1,7 @@
 package dev.paulee.api.plugin
 
+import java.awt.Color
+
 @Target(AnnotationTarget.CLASS)
 annotation class PluginOrder(val order: Int)
 
@@ -8,9 +10,13 @@ annotation class PluginMetadata(
     val name: String,
     val version: String = "",
     val author: String = "",
-    val description: String = ""
+    val description: String = "",
 )
 
 interface IPlugin {
     fun init()
+}
+
+interface Taggable {
+    fun tag(field: String, value: String): Map<String, Color>
 }
