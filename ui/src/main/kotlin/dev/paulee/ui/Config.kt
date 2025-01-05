00fa4783fc6d47bb72e9e30d33586abacf0bad55
Paul <41129807+Paulanerus@users.noreply.github.com>
2025-01-05 18:44:22 +0100

    feat: Added Config

---
+++ ui/src/main/kotlin/dev/paulee/ui/Config.kt
@@ -0,0 +1,65 @@
+package dev.paulee.ui
+
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.bufferedReader
+import kotlin.io.path.bufferedWriter
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.KVisibility
+import kotlin.reflect.full.memberProperties
+
+object Config {
+
+    var noWidthRestriction = false
+
+    private var configFile = "config"
+
+    private var configPath = Path(configFile)
+
+    fun save() {
+        this.configPath.bufferedWriter().use { writer ->
+
+            this::class.memberProperties
+                .filter { it.visibility == KVisibility.PUBLIC && it is KMutableProperty<*> }
+                .forEach {
+                    val value = it.getter.call(this)
+
+                    writer.write("${it.name} = $value\n")
+                    writer.newLine()
+                }
+        }
+    }
+
+    fun load(path: Path) {
+        this.configPath = path.resolve(configFile)
+
+        this.configPath.bufferedReader().useLines { lines ->
+            lines.filter { it.contains("=") }.forEach {
+                val (field, value) = it.split("=", limit = 2).map { it.trim() }
+
+                val member = this::class.memberProperties.find { it.name == field }
+
+                if (member != null && member is KMutableProperty<*>) {
+                    val converted: Any = when (member.returnType.classifier) {
+                        Boolean::class -> value.toBooleanStrictOrNull() ?: false
+                        Short::class -> value.toShortOrNull() ?: 0
+                        Int::class -> value.toIntOrNull() ?: 0
+                        Long::class -> value.toLongOrNull() ?: 0L
+                        Float::class -> value.toFloatOrNull() ?: 0f
+                        Double::class -> value.toDoubleOrNull() ?: 0
+                        else -> value
+                    }
+
+                    runCatching {
+                        member.setter.call(
+                            this,
+                            converted
+                        )
+                    }.onFailure { e -> println("Failed to set value for $field (${e.message}).") }
+                } else {
+                    //TODO
+                }
+            }
+        }
+    }
+}
