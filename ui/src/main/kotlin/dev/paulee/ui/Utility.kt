8308e7ee6559732a1274f8bd66dde2ba376b2c6d
Paul <41129807+Paulanerus@users.noreply.github.com>
2025-01-09 18:49:10 +0100

    feat: Added initial HeatmapText implementation

--- ui/src/main/kotlin/dev/paulee/ui/Utility.kt
+++ ui/src/main/kotlin/dev/paulee/ui/Utility.kt
@@ -1,5 +1,6 @@
 package dev.paulee.ui
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -7,9 +8,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
+import dev.paulee.api.data.Change
 import dev.paulee.api.plugin.Tag
 
 fun java.awt.Color.toComposeColor() = Color(red, green, blue, alpha)
@@ -18,6 +21,7 @@ fun java.awt.Color.toComposeColor() = Color(red, green, blue, alpha)
 fun MarkedText(
     modifier: Modifier = Modifier,
     textDecoration: TextDecoration = TextDecoration.None,
+    textAlign: TextAlign = TextAlign.Start,
     text: String,
     highlights: Map<String, Tag>,
 ) {
@@ -55,5 +59,39 @@ fun MarkedText(
 
         if (currentIndex < text.length) append(text.substring(currentIndex))
     }
-    Text(text = annotatedString, modifier = modifier, textDecoration = textDecoration)
+    Text(text = annotatedString, modifier = modifier, textDecoration = textDecoration, textAlign = textAlign)
+}
+
+@Composable
+fun HeatmapText(
+    change: Change?,
+    fallback: String,
+    modifier: Modifier = Modifier,
+    textDecoration: TextDecoration = TextDecoration.None,
+    textAlign: TextAlign = TextAlign.Center,
+) {
+    val text = change?.str ?: fallback
+
+    val annotatedString = buildAnnotatedString {
+        var currentIndex = 0
+
+        change?.tokens.orEmpty().forEach { (token, range) ->
+            val startIndex = text.indexOf(token, currentIndex)
+
+            val composeColor = if (token.startsWith("**")) Color.Green else Color.Red
+
+            if (startIndex != -1) {
+                append(text.substring(currentIndex, startIndex))
+
+                withStyle(style = SpanStyle(background = composeColor.copy(alpha = 0.3f))) {
+                    append(token)
+                }
+
+                currentIndex = startIndex + token.length
+            }
+        }
+
+        if (currentIndex < text.length) append(text.substring(currentIndex))
+    }
+    Text(text = annotatedString, modifier = modifier, textDecoration = textDecoration, textAlign = textAlign)
 }
