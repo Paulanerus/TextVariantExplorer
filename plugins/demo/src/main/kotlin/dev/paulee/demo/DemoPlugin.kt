237a352ba63b90f97bf8eb9c8957aeccda31e05f
Paul <41129807+Paulanerus@users.noreply.github.com>
2025-01-14 21:05:08 +0100

    feat: Updated demo plugin

--- plugins/demo/src/main/kotlin/dev/paulee/demo/DemoPlugin.kt
+++ plugins/demo/src/main/kotlin/dev/paulee/demo/DemoPlugin.kt
@@ -1,16 +1,20 @@
 package dev.paulee.demo
 
 import dev.paulee.api.data.RequiresData
+import dev.paulee.api.data.ViewFilter
 import dev.paulee.api.plugin.IPlugin
 import dev.paulee.api.plugin.PluginMetadata
-import dev.paulee.api.plugin.PluginOrder
+import dev.paulee.api.plugin.Tag
+import dev.paulee.api.plugin.Taggable
 
-@PluginOrder(4)
-@RequiresData(name = "demo", [Verse::class])
-@PluginMetadata(name = "Demo-Plugin", version = "1.0.0", author = "Paul")
-class DemoPlugin : IPlugin {
+@RequiresData(name = "greek_variant", [Occurrence::class, Name::class, Manuscript::class, Verse::class])
+@PluginMetadata(name = "GreekVariant-Plugin", version = "1.0.0", author = "Paul")
+class DemoPlugin : IPlugin, Taggable {
 
     override fun init() {
-        println("${greeting()} from Demo-Plugin")
+        println("Loaded GreekVariant Plugin")
     }
+
+    @ViewFilter("DemoTag", fields = ["text"], global = true)
+    override fun tag(field: String, value: String): Map<String, Tag> = emptyMap()
 }
