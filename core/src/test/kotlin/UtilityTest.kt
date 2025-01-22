0a947bd930a63ff8a67534bb3108f235c1ea45dc
Paul <41129807+Paulanerus@users.noreply.github.com>
2025-01-22 18:15:06 +0100

    test: JUnit tests for utility functions

---
+++ core/src/test/kotlin/UtilityTest.kt
@@ -0,0 +1,42 @@
+import dev.paulee.core.toCamelCase
+import dev.paulee.core.toSnakeCase
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class UtilityTest {
+    @Test
+    fun splitStr() {
+        val strOne = "The table 'Data List' has 121.432 entries"
+        val arrayOne = listOf("The", "table", "'Data List'", "has", "121.432", "entries")
+
+        val strTwo = "This_table_is_pretty_huge"
+        val arrayTwo = listOf("This", "table", "is", "pretty", "huge")
+
+        assertEquals(dev.paulee.core.splitStr(strOne, delimiter = ' ', quoteCharacters = arrayOf('\'')), arrayOne)
+        assertEquals(dev.paulee.core.splitStr(strTwo, delimiter = '_'), arrayTwo)
+    }
+
+    @Test
+    fun normalizeDataSource() {
+        val sourceOne = "file 1.csv"
+        val sourceTwo = "file 2"
+
+        assertEquals("file_1", dev.paulee.core.normalizeDataSource(sourceOne))
+
+        assertEquals("file_2", dev.paulee.core.normalizeDataSource(sourceTwo))
+    }
+
+    @Test
+    fun toSnakeCase() {
+        val str = "myFunction"
+
+        assertEquals("my_function", str.toSnakeCase())
+    }
+
+    @Test
+    fun toCamelCase() {
+        val str = "my_function"
+
+        assertEquals("MyFunction", str.toCamelCase())
+    }
+}
