4d25b27f2b648ae8ccef16fddafcc0488d0c39b2
Paul <41129807+Paulanerus@users.noreply.github.com>
2025-04-13 21:37:41 +0200

    feat: Enhance FileDialog with dialog type and extension filtering

--- ui/src/main/kotlin/dev/paulee/ui/components/Dialog.kt
+++ ui/src/main/kotlin/dev/paulee/ui/components/Dialog.kt
@@ -4,25 +4,52 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.AwtWindow
 import java.awt.FileDialog
 import java.awt.Frame
+import java.io.FilenameFilter
 import java.nio.file.Path
+
+enum class DialogType {
+    LOAD,
+    SAVE
+}
 
 @Composable
 fun FileDialog(
     parent: Frame? = null,
+    dialogType: DialogType = DialogType.LOAD,
+    extension: String? = null,
     onCloseRequest: (result: List<Path>) -> Unit
 ) = AwtWindow(
     create = {
-        object : FileDialog(parent, "Choose a file", LOAD) {
+        object :
+            FileDialog(parent, if (dialogType == DialogType.SAVE) "Save file" else "Open file", dialogType.ordinal) {
 
             init {
-                isMultipleMode = true
+                isMultipleMode = dialogType == DialogType.LOAD
+
+                if (!extension.isNullOrBlank()) {
+                    filenameFilter = FilenameFilter { _, name ->
+                        name.endsWith(".$extension", ignoreCase = true)
+                    }
+
+                    if (dialogType == DialogType.SAVE) file = "untitled"
+                }
             }
 
             override fun setVisible(value: Boolean) {
                 super.setVisible(value)
 
                 if (value) {
-                    val paths = files.map { it.toPath() }.toList()
+                    val paths = files.map {
+                        val path = it.toPath()
+
+                        if (dialogType == DialogType.SAVE && extension != null && !path.toString().lowercase()
+                                .endsWith(".$extension")
+                        ) {
+                            Path.of("${path}.$extension")
+                        } else {
+                            path
+                        }
+                    }.toList()
 
                     onCloseRequest(paths)
                 }
