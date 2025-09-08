1b8d6cab2fac28d095fb6dd089e1a0bf9de777d2
Paul <41129807+Paulanerus@users.noreply.github.com>
2025-09-08 20:12:57 +0200

    feat: Enhance Embedding model with file references

--- api/src/main/kotlin/dev/paulee/api/internal/Embedding.kt
+++ api/src/main/kotlin/dev/paulee/api/internal/Embedding.kt
@@ -1,12 +1,26 @@
 package dev.paulee.api.internal
 
 object Embedding {
-    enum class Models(val id: String, val description: String, val author: String, val parameter: String) {
+    enum class Models(
+        val id: String,
+        val description: String,
+        val author: String,
+        val parameter: String,
+        val modelFiles: ModelFiles,
+    ) {
         EmbeddingGemma(
             "onnx-community/embeddinggemma-300m-ONNX",
-            "A 300M parameter open embedding model from Google, built on Gemma 3 with T5Gemma initialization. Trained on 100+ languages, it creates text embeddings for tasks like search, classification, clustering, and semantic similarity.",
+            "A lightweight open embedding model from Google, built on Gemma 3 and trained on 100+ spoken languages.",
             "Google DeepMind",
-            "300M"
+            "300M",
+            ModelFiles()
         )
     }
+
+    data class ModelFiles(
+        val model: String = "model.onnx",
+        val modelData: String = "model.onnx_data",
+        val tokenizer: String = "tokenizer.json",
+        val tokenizerConfig: String = "tokenizer_config.json",
+    )
 }
